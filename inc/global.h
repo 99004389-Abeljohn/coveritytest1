c12de7b8f95c5969d756fd2c765de1a78d26f81f
99004389-Abeljohn <84492875+99004389-Abeljohn@users.noreply.github.com>
2022-01-24 00:21:29 -0500

    Add files via upload

---
+++ inc/global.h
@@ -0,0 +1,7 @@
+#ifndef EXTERN
+#define EXTERN extern
+#endif
+
+EXTERN char cell[82];
+EXTERN char wholecheck[10];
+EXTERN int block_array[9],player,block_number;
---
+++ inc/tictactoe.h
@@ -0,0 +1,125 @@
+/**
+ * @file tictactoe.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __TICTACTOE_H_
+#define __TICTACTOE_H_
+#include<stdio.h>
+/**
+ * @brief 
+ * 
+ */
+void board();
+/**
+ * @brief 
+ * 
+ */
+void block1();
+/**
+ * @brief 
+ * 
+ */
+void block2();
+/**
+ * @brief 
+ * 
+ */
+void block3();
+/**
+ * @brief 
+ * 
+ */
+void block4();
+/**
+ * @brief 
+ * 
+ */
+void block5();
+/**
+ * @brief 
+ * 
+ */
+void block6();
+/**
+ * @brief 
+ * 
+ */
+void block7();
+/**
+ * @brief 
+ * 
+ */
+void block8();
+/**
+ * @brief 
+ * 
+ */
+void block9();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int checkwin1();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int checkwin2();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int checkwin3();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int checkwin4();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int checkwin5();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int checkwin6();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int checkwin7();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int checkwin8();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int checkwin9();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int wholecheckwin();
+
+#endif
